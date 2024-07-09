38dbc4dc268abd96f9212ed770efcecdcabc7e9a
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2024-07-09 17:11:51 +0300

    Create bot_manage_webhook.py

---
+++ bot_manage_webhook.py
@@ -0,0 +1,25 @@
+# script to configure webhook for a bot
+import requests
+import argparse
+# Config import
+from QRcodebot_cfg import *
+
+def set_telegram_webhook(token, url, max_connections=28, drop_pending_updates=True):
+    telegram_api_url = f"https://api.telegram.org/bot{token}/setWebhook"
+    payload = {
+        'url': url,
+        'max_connections': max_connections,
+        'drop_pending_updates': drop_pending_updates
+    }
+    response = requests.post(telegram_api_url, data=payload)
+    return response.json()
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Set Telegram webhook.')
+    parser.add_argument('token', type=str, help='Telegram bot token')
+    parser.add_argument('url', type=str, help='Webhook URL')
+    
+    args = parser.parse_args()
+
+    response = set_telegram_webhook(args.token, args.url)
+    print(response)
