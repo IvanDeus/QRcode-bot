969b0b5819c734e0d7f6cbef7a571438a36a2d58
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2024-07-09 16:34:43 +0300

    Update run-my-bot.py

--- run-my-bot.py
+++ run-my-bot.py
@@ -1,11 +1,41 @@
 #python3 script to start bot process Telebot_store
 import sys
+import psutil
 #### BEGINNING ###
 # my path
 import os
 script_directory = os.path.dirname(os.path.abspath(__file__))
 # Config import
 from QRcodebot_cfg import *
+# status
+def filter_processes_by_port(bot_lport):
+  filtered_processes = []
+  pid_list = []
+  for process in psutil.process_iter():
+    try:
+      # Check connections for the desired port
+      for connection in process.connections():
+        if connection.laddr.port == bot_lport:
+          filtered_processes.append(process)
+          break  # Stop iterating connections if found
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+      pass  # Handle potential exceptions
+  pid_list = [process.pid for process in filtered_processes] 
+  return filtered_processes, pid_list
+# Now you can use the pid_list in your kill_processes function
+def kill_processes(pid_list):
+    for pid in pid_list:
+        try:
+            process = psutil.Process(pid)
+            process.terminate()
+            process.wait(timeout=3)  # Wait for the process to terminate
+            print(f"Successfully terminated process {pid}")
+        except psutil.NoSuchProcess:
+            print(f"No such process: {pid}")
+        except psutil.AccessDenied:
+            print(f"Access denied: {pid}")
+        except psutil.TimeoutExpired:
+            print(f"Timeout expired for process {pid}")
 # Help message
 if len(sys.argv) == 1:
     print("Usage: {} [start|stop|status]".format(sys.argv[0]))
