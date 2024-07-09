2a8c336a77d823997f193fb5c4bcf19cc71d714d
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2024-07-09 13:30:16 +0300

    Create QRcodebot_cfg_example.py

---
+++ QRcodebot_cfg_example.py
@@ -0,0 +1,19 @@
+# Ivan Deus telebot py cfg
+# Put
+# Credentials
+# below
+# define local port for webhook
+bot_lport = 5000
+# Database connection parameters
+db_host = "localhost"
+db_username = ""
+db_password = ""
+db_name = ""
+# MySQL socket path
+mysql_unix_socket = "/var/run/mysqld/mysqld.sock"
+# SMTP Server configuration
+smtp_server = 'localhost'
+smtp_port = 25
+#paths
+install_path = "/var/www"
+logfpath = "/var/log/QRbot_.log"
