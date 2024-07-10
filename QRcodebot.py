2da720b31857ea7f03bbfe4aa9b3ea1e07b877bb
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2024-07-10 08:59:16 +0300

    Update QRcodebot.py

--- QRcodebot.py
+++ QRcodebot.py
@@ -139,7 +139,7 @@ def get_level_for_user(conn, chat_id):
         result = None
         with conn.cursor() as cursor:
             # update user the record
-            s_query = f"SELECT level FROM telebot_users WHERE chat_id = '{chat_id}' limit 1"
+            s_query = f"SELECT level,lastmsg,u_url FROM telebot_users WHERE chat_id = '{chat_id}' limit 1"
             cursor.execute(s_query)
             result = cursor.fetchone()
             conn.commit()
